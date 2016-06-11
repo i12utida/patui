867658fc714b4b8162603054679d718b18550005
i12utida <i12utida@tokuyama.ac.jp>
2016-06-11 14:59:10 +0000

    Change yield function to full_title function

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,40 +1,45 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get product" do
     get :product
     assert_response :success
-    assert_select "title", "Product | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Product | #{@base_title}"
   end
 
   test "should get news" do
     get :news
     assert_response :success
-    assert_select "title", "News | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "News | #{@base_title}"
   end
 
   test "should get questionnaire" do
     get :questionnaire
     assert_response :success
-    assert_select "title", "Questionnaire | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Questionnaire | #{@base_title}"
   end
 
   test "should get stationery" do
     get :stationery
     assert_response :success
-    assert_select "title", "Stationery | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Stationery | #{@base_title}"
   end
 
   test "should get drink" do
     get :drink
     assert_response :success
-    assert_select "title", "Drink | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Drink | #{@base_title}"
   end
 
   test "should get noodle" do
@@ -46,13 +51,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get bread" do
     get :bread
     assert_response :success
-    assert_select "title", "Bread | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Bread | #{@base_title}"
   end
 
   test "should get lunch" do
     get :lunch
     assert_response :success
-    assert_select "title", "Lunch | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Lunch | #{@base_title}"
   end
 
 end
