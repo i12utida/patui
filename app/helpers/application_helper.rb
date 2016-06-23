ea40de1f974bbd6a792f92fedcc157b975836f9f
i12utida <i12utida@tokuyama.ac.jp>
2016-06-23 04:44:15 +0000

    Flash user signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     if page_title.empty?                              # 論理値でテスト
       base_title                                      # 暗黙的な返り値
     else
-      page_title + " | " + base_title                 # 文字列の結合
+      page_title                                      #ページタイトル
     end
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,67 +3,67 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+  #  @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Patui" #{@base_title}
   end
 
   test "should get product" do
     get :product
     assert_response :success
-    assert_select "title", "Product | #{@base_title}"
+    assert_select "title", "Product" #{@base_title}
   end
 
   test "should get news" do
     get :news
     assert_response :success
-    assert_select "title", "News | #{@base_title}"
+    assert_select "title", "News" #{@base_title}
   end
 
   test "should get questionnaire" do
     get :questionnaire
     assert_response :success
-    assert_select "title", "Questionnaire | #{@base_title}"
+    assert_select "title", "Questionnaire" #{@base_title}
   end
   
    test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact" #{@base_title}
   end
 
   test "should get stationery" do
     get :stationery
     assert_response :success
-    assert_select "title", "Stationery | #{@base_title}"
+    assert_select "title", "Stationery" #{@base_title}
   end
 
   test "should get drink" do
     get :drink
     assert_response :success
-    assert_select "title", "Drink | #{@base_title}"
+    assert_select "title", "Drink" #{@base_title}
   end
 
   test "should get noodle" do
     get :noodle
     assert_response :success
-    assert_select "title",  "Noodle | Ruby on Rails Tutorial Sample App"
+    assert_select "title",  "Noodle" #{@base_title}
   end
 
   test "should get bread" do
     get :bread
     assert_response :success
-    assert_select "title", "Bread | #{@base_title}"
+    assert_select "title", "Bread" #{@base_title}
   end
 
   test "should get lunch" do
     get :lunch
     assert_response :success
-    assert_select "title", "Lunch | #{@base_title}"
+    assert_select "title", "Lunch" #{@base_title}
   end
 
 end
