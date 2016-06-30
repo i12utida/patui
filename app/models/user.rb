cdf885c18a76a463c2e33e234942d332e17cef03
i12utida <i12utida@tokuyama.ac.jp>
2016-06-30 06:33:00 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
     before_save { self.email = email.downcase}
     
     #gakusekiは数字5つの場合のみ許可する
@@ -16,5 +17,35 @@ class User < ActiveRecord::Base
     has_secure_password
     
     #passwordの空白を不可、長さは6文字以上
-    validates(:password, presence: true, length: {minimum: 6})
+    validates(:password, presence: true, length: {minimum: 6}, allow_nil: true)
+    
+    # 与えられた文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+    
+     # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続的セッションで使用するユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーログインを破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
 end
